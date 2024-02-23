5b455feea15223c4a422b8d629b9a21421418525
Richard Marmito <richardmarmito@gmail.com>
2024-02-23 10:20:56 -0800

    Initialized MUI Framework. Implemented test header.

--- dev-bubblemachine/bubblemachine-react/src/App.js
+++ dev-bubblemachine/bubblemachine-react/src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
+import React from "react";
+
+import Header from "./components/Header.js"; // Make sure the path is correct
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>BubbleMachine</p>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
