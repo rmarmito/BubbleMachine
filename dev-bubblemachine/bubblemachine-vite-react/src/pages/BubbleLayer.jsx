9d2ccea14d615f71c964ddc40916c386604b8ef5
JamalBhola <78753034+JamalBhola@users.noreply.github.com>
2024-04-10 22:58:35 -0700

    Example hard code bubble render

--- dev-bubblemachine/bubblemachine-vite-react/src/pages/BubbleLayer.jsx
+++ dev-bubblemachine/bubblemachine-vite-react/src/pages/BubbleLayer.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BubbleBox from "../components/BubbleBox";
 import { Box } from "@mui/material";
+import { Stage, Layer, Ellipse } from 'react-konva'
 
 const BubbleLayer = () => {
+
+  const [stageWidth, setStageWidth] = useState(window.innerWidth);
+  const [stageHeight, setStageHeight] = useState(window.innerHeight);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setStageWidth(window.innerWidth);
+      setStageHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); 
+
   return (
     <div>
       <BubbleBox
@@ -18,10 +36,110 @@ const BubbleLayer = () => {
             borderColor: "black",
             borderRadius: "10px",
             backgroundColor: "white",
-            p: 20,
+            p: 5,
           }}
         >
-          Bubbles will go here :)
+          <Stage width = {1750} height ={500} >
+            <Layer>
+              <Ellipse 
+                x = {100} 
+                y = {500}
+                width = {200}
+                height = {300}
+                fill = "Blue"
+                stroke = "black" 
+              />
+              <Ellipse 
+                x = {200} 
+                y = {500}
+                width = {400}
+                height = {100}
+                fill = "red"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {400} 
+                y = {500}
+                width = {400}
+                height = {300}
+                fill = "blue"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {600} 
+                y = {500}
+                width = {500}
+                height = {500}
+                fill = "green"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {725} 
+                y = {500}
+                width = {50}
+                height = {100}
+                fill = "red"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {200} 
+                y = {500}
+                width = {400}
+                height = {100}
+                fill = "red"
+                stroke = "black"  
+              />
+              {/* Second section */}
+              <Ellipse 
+                x = {1000} 
+                y = {500}
+                width = {400}
+                height = {300}
+                fill = "blue"
+                stroke = "black" 
+              />
+              <Ellipse 
+                x = {700} 
+                y = {500}
+                width = {200}
+                height = {300}
+                fill = "blue"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {400} 
+                y = {500}
+                width = {400}
+                height = {300}
+                fill = "blue"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {1500} 
+                y = {500}
+                width = {300}
+                height = {100}
+                fill = "red"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {1300} 
+                y = {500}
+                width = {300}
+                height = {50}
+                fill = "yellow"
+                stroke = "black"  
+              />
+              <Ellipse 
+                x = {200} 
+                y = {500}
+                width = {400}
+                height = {100}
+                fill = "red"
+                stroke = "black"  
+              />
+            </Layer>
+          </Stage> 
         </Box>
       </BubbleBox>
     </div>
