605f46fb5f4bb91ae96687a0b21697a051080b18
Leanne Allen <leanne.allenm@gmail.com>
2024-06-06 11:35:56 -0700

    First commit

--- dev-bubblemachine/bubblemachine-vite-react/src/main.jsx
+++ dev-bubblemachine/bubblemachine-vite-react/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+//import "./index.css";
 
 const theme = createTheme({
   // You can customize your theme here
