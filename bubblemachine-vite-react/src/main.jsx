79ce61a631dc508e4c91d40efeb9e1e887bc74f2
rmarmito <102631640+rmarmito@users.noreply.github.com>
2024-09-06 15:08:49 -0700

    Removed nested folders. Commented compiler issues related to waveform visualizer

---
+++ bubblemachine-vite-react/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+//import "./index.css";
+
+const theme = createTheme({
+  // You can customize your theme here
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
