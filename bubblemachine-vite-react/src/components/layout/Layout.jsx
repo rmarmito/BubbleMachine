743bc8aecf41e7f68f2376c31f9747a114c5cfdf
rmarmito <102631640+rmarmito@users.noreply.github.com>
2024-10-17 11:45:57 -0700

    Further restructure file hierarchy

--- bubblemachine-vite-react/src/components/layout/Layout.jsx
+++ bubblemachine-vite-react/src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 // src/pages/Layout.jsx
 import React from "react";
-import Header from "../components/layout/Header";
-import Footer from "../components/layout/Footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import { Outlet, Link } from "react-router-dom";
 
 export default function Layout() {
