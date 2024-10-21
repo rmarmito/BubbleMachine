a3b93cc3c80e1d4775bfcf12a0de117d267f6152
JamalBhola <jbhola4837@sdsu.edu>
2024-10-21 13:43:00 -0700

    Zoom and pan works, preliminary testing shows no issues, requires further testing

--- bubblemachine-vite-react/src/components/bubbles/BubbleRender.jsx
+++ bubblemachine-vite-react/src/components/bubbles/BubbleRender.jsx
@@ -3,28 +3,45 @@ import useBubbleStore from '../zustand/bubbleStore';
 import { convertToMilliseconds, colorToHex} from '../../helpers/utils';
 
 //import './BubbleRender.css'; // Assuming you have a CSS file for additional styling
-const BubbleRender = ({ audioDuration = 0, vizWidth = 800}) => {
+const BubbleRender = ({ audioDuration = 0, vizWidth = 800, visibleStartTime=0, visibleEndTime = audioDuration}) => {
     const bubbleData = useBubbleStore((state) => state.bubbles);
+    console.log('visibleStartTime', visibleStartTime);
+    console.log('visibleEndTime', visibleEndTime);
+    const visStartMs = convertToMilliseconds(visibleStartTime);
+    const visStopMs = convertToMilliseconds(visibleEndTime);
+
 
     return (
         // original height: 300px
         <div style={{ position: 'relative', width: '100%', height: '200px' }}> 
             {console.log('bubbleData', bubbleData)}
             {bubbleData.map((bubbleData, index) => {
+                console.log('startTime', bubbleData.startTime);
+                console.log('stopTime', bubbleData.stopTime);
                 const startTime = convertToMilliseconds(bubbleData.startTime);
                 const stopTime = convertToMilliseconds(bubbleData.stopTime);
                 const bubbleColor = colorToHex(bubbleData.color);
-                const audioLength = Math.floor(audioDuration * 1000);
+                if(startTime > visStopMs || stopTime < visStartMs) {
+                    return null;
+                }
+                
+                if(visibleStartTime === 0 && visibleEndTime === 0) {
+                    const audioLength = Math.floor(audioDuration * 1000);
 
-                // Compute the bubble's start position
-                const startPosition = Math.floor((startTime / audioLength * vizWidth)+20);
-                const startPositionChecked = isNaN(startPosition) ? 0 : startPosition;
+                    // Compute the bubble's start position
+                    const preStartPosition = Math.floor((startTime / audioLength * vizWidth)+20);
+                    var startPosition = isNaN(preStartPosition) ? 0 : preStartPosition;
 
-                // Compute the bubble's width
-                const bubbleWidth = Math.floor((stopTime - startTime) / audioLength * vizWidth);
-                const defaultBubbleWidth = 15;
-                const bubbleWidthChecked = isNaN(bubbleWidth) || bubbleWidth === 0 ? defaultBubbleWidth : bubbleWidth;
-
+                    // Compute the bubble's width
+                    const preBubbleWidth = Math.floor((stopTime - startTime) / audioLength * vizWidth);
+                    const defaultBubbleWidth = 15;
+                    var bubbleWidth = isNaN(preBubbleWidth) || preBubbleWidth === 0 ? defaultBubbleWidth : preBubbleWidth;
+                } else {
+                    const visibleDuration = visStopMs - visStartMs;
+                    var startPosition = Math.max(0, (startTime - visStartMs) / visibleDuration * vizWidth)+20;
+                    var endPosition = Math.min(vizWidth, (stopTime - visStartMs) / visibleDuration * vizWidth);
+                    var bubbleWidth = endPosition - startPosition;
+                }
                 // Convert level to a pixel height
                 const bubbleHeight = bubbleData.layer * 50;
 
@@ -36,8 +53,8 @@ const BubbleRender = ({ audioDuration = 0, vizWidth = 800}) => {
                     bottom: 0,
                     color: bubbleColor,
                     backgroundColor: bubbleColor,
-                    left: `${startPositionChecked}px`,
-                    width: `${bubbleWidthChecked}px`,
+                    left: `${startPosition}px`,
+                    width: `${bubbleWidth}px`,
                     height: `${bubbleHeight}px`,
                     zIndex: bubbleLevel,
                     position: 'absolute',
